d10d25cb34413580757435d615f46d77ddf36f7e
I447791 <z.zhang@fontys.student.nl>
2020-12-31 00:20:10 +0100

    completed documentation and added trained table gan file

--- TrainedTableGan.py
+++ TrainedTableGan.py
@@ -13,16 +13,12 @@ def getSavePath(path, name):
                 number = int(file.split("_")[2][0:1])
             except:   
                 number = 0
-
+                
             if(number > index):
                 index = number + 1
 
     return path + "/" + name + "_" + str(index) + ".csv"
 
-
-# importing a custom dataset which must be a pandas.dataframe or csv file
-data = pd.read_csv('data/adult_cleared.csv')
-data.drop(['Unnamed: 0'], axis=1, inplace=True)
 
 # generate dinamically names for syntethic dataset (e.g. "synthetic_adult_1" , "synthetic_adult_2")
 pathToSave = getSavePath("Synthetic_data", "synthetic_adult")
@@ -30,38 +26,16 @@ pathToSave = getSavePath("Synthetic_data", "synthetic_adult")
 # number of samples that we desire to generate
 num_samples = 400
 
-# model save location
+# trained model location
 model_path = 'models/Adult_2.pkl'
 
-# the TGAN model need to know which dataset columns are the type of continuous columns. 
-continuous_columns = [0, 2, 4, 10, 11, 12]
-
-# set nn parameters, like epoch, batch size and loss function
-tgan = TGANModel(continuous_columns,
-                  output='output',
-                  max_epoch=10,
-                  steps_per_epoch=400,
-                  save_checkpoints=True,
-                  restore_session=False,
-                  batch_size=200,
-                  z_dim=200,
-                  noise=0.2,
-                  l2norm=0.00001,
-                  learning_rate=0.001,
-                  num_gen_rnn=100,
-                  num_gen_feature=100,
-                  num_dis_layers=1,
-                  num_dis_hidden=100,
-                  optimizer='AdamOptimizer')
-                  
-# train phase 
-tgan.fit(data)
-
-print("Fitted model!!!")
+# load tgan model that was previously trained
+tgan = TGANModel.load(model_path)
 
 # after fitting, we can sample some new synthetic data which is a pandas.DataFrame
 samples = tgan.sample(num_samples)
 
+print(pathToSave)
 print(samples)
 
 samples.head()
